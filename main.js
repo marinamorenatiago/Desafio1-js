6ab1f6e978f2d533a8e1b5ce03d4586af0cff1e4
marinamorenatiago <morenatiago@gmail.com>
2021-05-27 19:32:03 -0300

    subindo desafio 1 js

---
+++ main.js
@@ -0,0 +1,21 @@
+let nome = "Marina"
+let idade = 21
+let anoatual = 2021
+
+console.log(`Meu ano de nascimento é ${anoatual-idade}`)
+
+let souhumano = true
+let naosouhumano = false
+
+if(anoatual == 2021) {
+    console.log(souhumano)
+}
+else {
+    console.log(naosouhumano)
+}
+
+let x = 35
+let y = 57
+
+console.log(`${x+y}`)
+
